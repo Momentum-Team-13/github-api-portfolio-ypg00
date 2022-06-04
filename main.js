b5cd442323c363e1f1e856dbae2546e9023ea9f4
Yuriy Golota <ypgolota@pm.me>
2022-06-04 09:49:27 -0400

    refactored everything and loop to create most divs, with ids, and appendchilds is broken

--- main.js
+++ main.js
@@ -1,37 +1,109 @@
-const gProfile = document.getElementById('profile');
-const gitHubUrl = 'https://api.github.com/users/ypg00';
+// Global Variables
+const gitHubUserName = 'ypg00';
 
-fetch(gitHubUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-})
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      console.log('You have been successfully subscribed', data)
-      createProfileElement(data);
-    });
+const profile = document.getElementById('profile');
+const gitHubProfileUrl = `https://api.github.com/users/${gitHubUserName}`;
+const gitHubReposURL = `https://api.github.com/users/${gitHubUserName}/repos`;
+let data;
 
-function createProfileElement (d) {
-    // Profile Pic
-    let gPic = document.createElement('img');
-    gPic.id = 'pic';
-    gPic.src = d.avatar_url;
-    gProfile.appendChild(gPic);
+console.log(`gitHubProfileUrl: ${gitHubProfileUrl}`);
+console.log(`gitHubReposUrl: ${gitHubReposURL}`);
 
-    // Name
-    let gName =  document.createElement('div')
-    gName.id = 'name';
-    gName.innerText = d.name;
-    gProfile.appendChild(gName);
+const importObjects = ['avatar_url', 'name', 'location', 'html_url', 'login'];
+console.log(`importObjects: ${importObjects}`);
 
-    // Location
+createGitHubProfilePage(gitHubUserName);
 
-    // GitHub URL
+function createGitHubProfilePage(gitHubUserName) {
+  
+  // fetch(GET) request with GitHub's API
+  fetch(gitHubProfileUrl, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+  })
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        console.log(`API fetch was successful. Data: ${data}`);
+        createProfile(data);
+      });
+}
+  // Creating HTML elements
 
-    // GitHub Username
+function createProfile (data) {
 
-    // GitHub Repos
+  // Profile pic
+  let pic = document.createElement('img');
+  pic.id = `${importObjects[0]}`;
+  pic.src = data[importObjects[0]];
+  profile.appendChild(pic);
+
+  // Creating HTML elements (<div>), variables, ids, and appendingChild for the majority of content
+  // createDivs(importObjects.slice(1));
+  console.log(`importObjects.slice(1): ${importObjects.slice(1)}`)
+  console.log(`line 45: data: ${data[importObjects.slice(1)]}`);
+
+  console.log(`data[importObjects[1]]: ${data[importObjects[1]])}`; 
+
+  for (let d of data[`${importObjects.slice(1)}`]) {
+    console.log(`d: ${d}`);
+    d = document.createElement('div');
+    // d.id = `${importObjects[d]}`;
+    console.log(`id: ${d.id}`);
+    profile.appendChild(importObjects[d]);
+  }
+
 
 }
+
+
+// Supporting functions
+
+// Creating a div element + id + append
+
+// function createDivs(keys) {
+//   console.log(`keys: ${keys}`);
+//   for (let key of keys) {
+//     let key = document.createElement('div');
+//     key.id = data[importObjects.slice(1)];  
+//     console.log(`key.id ${key.id}`)
+//     console.log(`key: ${importObject}`);
+//     profile.appendChild(key);
+//   }
+// } 
+
+    
+
+    
+// // Profile Pic
+// let pic = document.createElement('img');
+// pic.id = 'pic';
+// pic.src = d.avatar_url;
+// profile.appendChild(pic);
+
+// // Name
+// let name =  document.createElement('div');
+// name.id = 'name';
+// name.innerText = d.name;
+// profile.appendChild(name);
+
+// // Location
+// let gLocation = document.createElement('div');
+// gLocation.id = 'location';
+// gLocation.innerHTML = `<strong>Location:</strong> ${d.location}`;
+// profile.appendChild(gLocation);
+
+// // GitHub URL
+// let gURL = document.createElement('div');
+// gURL.id = 'url';
+// gURL.innerHTML = `<strong>GitHub URL:</strong> <a href=${d.html_url}>${d.html_url.slice(8)}</a>`;
+// profile.appendChild(gURL);
+
+// // GitHub Username
+// let gUserName = document.createElement('div');
+// gUserName.id = 'userName';
+// gUserName.innerHTML = `<strong>GitHub Username:</strong> ${d.login}`;
+// profile.appendChild(gUserName);
+
+  
